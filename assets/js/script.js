27ec7fc9cfd64559921855c99682536af2fe5e8e
Justin-Brueske <119259409+Justin-Brueske@users.noreply.github.com>
2023-01-21 13:09:56 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var latitude = "";
 var longitude = "";
 
 function getWeather() {
-    let city = cityEl.value.trim();
+    let city = cityEl.value;
     let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIkey;
     cityEl.value = "";
 
@@ -17,12 +17,16 @@ function getWeather() {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
+        // console.log(data);
         latitude = data[0].lat
         longitude = data[0].lon
         console.log(data[0].name);
-        console.log(data[0].lat);
-        console.log(data[0].lon);
+        searchedCities.unshift(data[0].name);
+        console.log(searchedCities);
+        localStorage.setItem("cityHistory", JSON.stringify(searchedCities));
+        renderCities();
+        // console.log(data[0].lat);
+        // console.log(data[0].lon);
         let weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude +"&lon=" + longitude + "&appid=" + APIkey + "&units=imperial";
         
         fetch(weatherURL)
@@ -30,16 +34,14 @@ function getWeather() {
             return response.json();
           })
           .then(function (data) {
-            console.log(data);
-            console.log(data.main.temp);
-            console.log(data.main.feels_like);
-            console.log(data.main.temp_min);
-            console.log(data.main.temp_max);
+            // console.log(data);
+            // console.log(data.main.temp);
+            // console.log(data.main.feels_like);
+            // console.log(data.main.temp_min);
+            // console.log(data.main.temp_max);
             let currentWeather = '<h3 class="text-3xl">' + data.name + '</h3> <ul class="list-none"> <li>Temperature: ' + data.main.temp + '&#8457;</li> <li>Feels like: ' + data.main.feels_like + '&#8457;</li> <li>Humidity: ' +  data.main.humidity + '%</li> </ul>';
             $("#current-weather").html(currentWeather);
           });
-
-
 
           let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude +"&lon=" + longitude + "&appid=" + APIkey + "&units=imperial";
           fetch(forecastURL)
@@ -47,17 +49,17 @@ function getWeather() {
               return response.json();
             })
             .then(function (data) {
-              console.log(data);
+            //   console.log(data);
               forecastEl.html("");
               let i = 0;
               let k = 0;
               do {               
-                console.log(data.list[i].dt_txt);
-                console.log(data.list[i].dt);
-                console.log(data.list[i].main.temp_max);
-                console.log(data.list[i].main.temp_min);
-                console.log(data.list[i].weather[0].description);
-                console.log(i);
+                // console.log(data.list[i].dt_txt);
+                // console.log(data.list[i].dt);
+                // console.log(data.list[i].main.temp_max);
+                // console.log(data.list[i].main.temp_min);
+                // console.log(data.list[i].weather[0].description);
+                // console.log(i);
                 let forecastDay = (new Date(data.list[i].dt*1000).toDateString());
                 let forecast = '<div><span class="text-2xl">' + forecastDay + '</span> <ul class="list-none"> <li>High: ' + data.list[i].main.temp_max + '&#8457;</li> <li>Low: ' + data.list[i].main.temp_min + '&#8457;</li> <li>Weather: ' + data.list[i].weather[0].description + '</li> </ul><div>';
                 forecastEl.append(forecast);
@@ -69,3 +71,26 @@ function getWeather() {
 };
 
 searchBtn.on("click", getWeather);
+
+function renderCities() {
+    searchedCityEl.html("");
+    for (let index = 0; index < searchedCities.length; index++) {
+        const historyCity = searchedCities[index];
+        console.log(searchedCities[index]);
+        var li = document.createElement('li');
+        li.textContent = historyCity;
+        searchedCityEl.append(li);        
+    } 
+}
+
+
+function init() {
+    const cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
+    if (cityHistory !== null) {
+        searchedCities = cityHistory;      
+    }
+    renderCities();
+}
+
+
+init()
